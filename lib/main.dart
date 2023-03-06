6b309232741ee2891d263cdce9ed70e98461dc3c
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-03-05 21:10:47 -0300

    feat(main.dart): Initialized Firebase SDK

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await FirebaseAppCheck.instance.activate();
+
   runApp(const MyApp());
 }
 

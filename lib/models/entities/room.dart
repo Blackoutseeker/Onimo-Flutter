9e12c5744061b6438b2425b41a4e5d1ac232d7e1
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-03-17 15:21:39 -0300

    feat(models): Created convertToDatabase method in Room class

--- lib/models/entities/room.dart
+++ lib/models/entities/room.dart
@@ -18,4 +18,18 @@ class Room {
   final String id;
   final String name;
   final List<ActiveUser> activeUsers;
+
+  Map<dynamic, dynamic> convertToDatabase() {
+    return {
+      'id': id,
+      'name': name,
+      'active_users': activeUsers.map(
+        (user) => {
+          user.id: {
+            'status': user.status,
+          }
+        },
+      ),
+    };
+  }
 }

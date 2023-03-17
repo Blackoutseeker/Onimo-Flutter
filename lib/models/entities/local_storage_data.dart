0096aee926d96d60f2d8367697228ca55e1e45f9
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-03-17 04:30:43 -0300

    feat(models): Created LocalStorageData entity

---
+++ lib/models/entities/local_storage_data.dart
@@ -0,0 +1,11 @@
+class LocalStorageData {
+  LocalStorageData({
+    required this.userId,
+    required this.userNickname,
+    required this.lastSessionDate,
+  });
+
+  String userId;
+  String userNickname;
+  String lastSessionDate;
+}

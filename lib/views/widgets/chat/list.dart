4f5d8cf9acfdf0a2a8360713338aeebf61ad31d9
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-03-17 00:23:46 -0300

    feat(views): Added auto scroll to chat

--- lib/views/widgets/chat/list.dart
+++ lib/views/widgets/chat/list.dart
@@ -18,13 +18,16 @@ class MessagesList extends StatefulWidget {
 
 class _MessagesListState extends State<MessagesList> {
   final FirebaseDatabase _firebaseDatabase = FirebaseDatabase.instance;
+  final ScrollController _scrollController = ScrollController();
 
   List<Message> _messages = [];
   late StreamSubscription<DatabaseEvent> _messagesSubscription;
+  bool _needToScroll = false;
 
   void _setMessagesState(List<Message> messages) {
     setState(() {
       _messages = messages;
+      _needToScroll = true;
     });
   }
 
@@ -48,6 +51,16 @@ class _MessagesListState extends State<MessagesList> {
     _messagesSubscription.cancel();
   }
 
+  void _scrollToEnd() {
+    if (_scrollController.positions.isNotEmpty) {
+      _scrollController.animateTo(
+        _scrollController.position.minScrollExtent,
+        duration: const Duration(milliseconds: 250),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -57,6 +70,7 @@ class _MessagesListState extends State<MessagesList> {
   @override
   void dispose() {
     _stopChatListener();
+    _scrollController.dispose();
     super.dispose();
   }
 
@@ -66,8 +80,16 @@ class _MessagesListState extends State<MessagesList> {
       return const BlankChat();
     }
 
+    if (_needToScroll) {
+      WidgetsBinding.instance.addPostFrameCallback(
+        (_) => _scrollToEnd(),
+      );
+      _needToScroll = false;
+    }
+
     return Expanded(
       child: ListView.separated(
+        controller: _scrollController,
         shrinkWrap: true,
         reverse: true,
         padding: const EdgeInsets.symmetric(horizontal: 20),

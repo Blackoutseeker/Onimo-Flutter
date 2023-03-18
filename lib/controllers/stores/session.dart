c079f204bc968d9f9ef4faf9a346dd9aeb4fe286
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-03-18 16:22:09 -0300

    feat(controllers): Created SessionStore class

---
+++ lib/controllers/stores/session.dart
@@ -0,0 +1,27 @@
+import 'package:mobx/mobx.dart';
+
+import 'package:onimo/models/entities/session.dart';
+
+part 'session.g.dart';
+
+class SessionStore = SessionStoreModel with _$SessionStore;
+
+abstract class SessionStoreModel with Store {
+  @observable
+  Session session = Session(
+    userId: 'temp_uid',
+    userNickname: 'john_doe7',
+    lastSessionDate: '2021-05-01 19:00:00',
+  );
+
+  @action
+  void setSession(Session session) {
+    this.session = session;
+  }
+
+  @action
+  void updateCurrentSessionRoom(String roomId, RoomType roomType) {
+    session.currentRoomId = roomId;
+    session.currentRoomType = roomType;
+  }
+}

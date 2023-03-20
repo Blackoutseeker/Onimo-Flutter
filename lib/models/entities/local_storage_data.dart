d717d71b5e8ff2cb18c0a3abf3e08b4c0923fc1d
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-03-20 02:34:47 -0300

    feat(models): Created toMap method in class LocalStorageData

--- lib/models/entities/local_storage_data.dart
+++ lib/models/entities/local_storage_data.dart
@@ -8,4 +8,12 @@ class LocalStorageData {
   String userId;
   String userNickname;
   String lastSessionDate;
+
+  Map<dynamic, dynamic> toMap() {
+    return {
+      'user_id': userId,
+      'user_nickname': userNickname,
+      'last_session_date': lastSessionDate,
+    };
+  }
 }

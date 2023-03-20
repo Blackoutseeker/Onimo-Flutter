95a6ffb808e879f38640105f8846a19b78b5ff79
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-03-20 02:44:34 -0300

    feat(main.dart): Added SessionManager class

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:cryptography_flutter/cryptography_flutter.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:get_it/get_it.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:flutter/services.dart';
 
 import './controllers/stores/session.dart';
+import './controllers/services/session_manager.dart';
 import './models/themes/default_theme.dart';
 import './views/screens/rooms.dart';
 
@@ -20,6 +21,8 @@ void main() async {
 
   final GetIt getIt = GetIt.I;
   getIt.registerLazySingleton<SessionStore>(() => SessionStore());
+
+  await SessionManager.initialize();
 
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
